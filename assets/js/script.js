0e65fbf75076f78fda6ed90e85a3056eca7a536e
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-08-30 14:54:34 -0400

    added buttons for test answers and styled them in a grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-alert("hello world")
+var startBtn = document.getElementById('start');
+var timerEl = document.getElementById('countdown');
+
+//var questions = ;
+
+//coutdown timer
+function countdown() {
+    var timeLeft = 5;
+
+    var timeInterval = setInterval(function() {
+        if (timeLeft >=1) {
+            timerEl.textContent = timeLeft +' Seconds remaining ';
+            timeLeft--;
+        }
+        else if ( timeLeft ===0){
+            timerEl.textContent =' Test Finished ';
+        }
+        else{
+            timerEl.textContent +'';
+            clearInterval(timeInterval);
+        }
+    },1000);
+}
+
+var score = timeLeft;
+var timeLeft = 5;
+
+//start button
+startBtn.onclick = countdown;

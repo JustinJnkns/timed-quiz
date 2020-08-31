2caf3cb4ad8c292ac7752b113e1fa50e30e7d506
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-08-30 21:25:17 -0400

    inserted answer  and questions from array into webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,83 @@
-var startBtn = document.getElementById('start');
-var timerEl = document.getElementById('countdown');
+const startBtn = document.getElementById('start');
+const nextButton = document.getElementById("next");
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement =  document.getElementById('answer-buttons')
 
-//var questions = ;
+let shuffledQuestions, currentQuestionIndex
+
+startBtn.addEventListener('click', startGame)
+
+
+
+function startGame() {
+    startBtn.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    questionContainerElement.classList.remove('hide')
+    currentQuestionIndex = 0
+ 
+    setNextQuestion()
+}
+
+function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+    questionElement.innerText = question.question
+    question.answers.forEach((answer) => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
+}
+
+
+
+function selectAnswer(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+const questions = [
+    {
+        question: 'What makes a webpages skeleton',
+        answers: [
+            { text: 'HTML', correct: true },
+            { text: 'JavaScript', correct: false }
+        ]
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //coutdown timer
 function countdown() {
@@ -22,8 +98,11 @@ function countdown() {
     },1000);
 }
 
+
+
 var score = timeLeft;
 var timeLeft = 5;
 
 //start button
-startBtn.onclick = countdown;
+//startBtn.onclick = countdown;
+var timerEl = document.getElementById('countdown');
